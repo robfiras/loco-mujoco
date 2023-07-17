1d795259bcfe42c69ded4122418202c7482cfbe7
Firas Al-Hafez <f.al-hafez@tu-braunschweig.de>
2023-07-17 22:52:04 +0200

    This commit includes changes, which should have been in the previous commit.

--- loco_mujoco/environments/humanoids/atlas.py
+++ loco_mujoco/environments/humanoids/atlas.py
@@ -37,7 +37,7 @@ class Atlas(LocoEnv):
             assert disable_arms is True, "If you want Atlas to carry a weight, please disable the arms. " \
                                          "They will be kept fixed."
 
-        xml_path = (Path(__file__).resolve().parent.parent / "data" / "atlas" / "model.xml").as_posix()
+        xml_path = (Path(__file__).resolve().parent.parent / "data" / "atlas" / "atlas.xml").as_posix()
 
         action_spec = self._get_action_specification()
 
