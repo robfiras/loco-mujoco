638132aca5027425e1bfa1d576c71d318f0781e0
robfiras <f.al-hafez@posteo.de>
2023-10-02 13:24:05 +0200

    fix wrong dependency.

---
+++ examples/domain_randomization/example_humanoid_torque.py
@@ -0,0 +1,22 @@
+import numpy as np
+from loco_mujoco import LocoEnv
+
+
+env = LocoEnv.make("HumanoidTorque", domain_randomization_config="./humanoid.yaml")
+
+action_dim = env.info.action_space.shape[0]
+
+env.reset()
+env.render()
+absorbing = False
+i = 0
+
+while True:
+    if i == 1000 or absorbing:
+        env.reset()
+        i = 0
+    action = np.random.randn(action_dim)
+    nstate, _, absorbing, _ = env.step(action)
+
+    env.render()
+    i += 1
