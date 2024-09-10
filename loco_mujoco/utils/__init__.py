2fc1907295a6a90e6e779cbce6b8ec6ffb89859f
robfiras <f.al-hafez@posteo.de>
2024-09-10 14:45:22 +0200

    removed old data conversion scripts.

--- loco_mujoco/utils/__init__.py
+++ loco_mujoco/utils/__init__.py
@@ -3,4 +3,5 @@ from .trajectory import *
 from .checks import *
 from .video import video2gif
 from .domain_randomization import *
+from .myomodel_init import fetch_myoskeleton, clear_myoskeleton
 from .dataset import download_all_datasets, download_real_datasets, download_perfect_datasets
