59aaaa337686d0e059d8ff323c297b3ace4de5fc
p756017 <sarah.prahl@tu-dortmund.de>
2026-05-15 19:28:16 +0200

    Added LocoEnv: Mjx Unitree G1 with Inspire Hands

--- loco_mujoco/environments/humanoids/__init__.py
+++ loco_mujoco/environments/humanoids/__init__.py
@@ -8,6 +8,7 @@ from .unitreeH1v2 import UnitreeH1v2
 from .unitreeH1v2_mjx import MjxUnitreeH1v2
 from .unitreeG1 import UnitreeG1
 from .unitreeG1_inspire import UnitreeG1Inspire
+from .unitreeG1_inspire_mjx import MjxUnitreeG1Inspire
 from .myoskeleton import MyoSkeleton
 from .myoskeleton_mjx import MjxMyoSkeleton
 from .unitreeG1_mjx import MjxUnitreeG1
@@ -35,6 +36,7 @@ MjxUnitreeH1v2.register()
 UnitreeG1.register()
 UnitreeG1Inspire.register()
 MjxUnitreeG1.register()
+MjxUnitreeG1Inspire.register()
 Apollo.register()
 MjxApollo.register()
 BoosterT1.register()
